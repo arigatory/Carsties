4bfc26446ef8218371d0f7fa8016f1479300d06c
Ivan Panchenko <arigatory@gmail.com>
2023-09-24 22:44:05 +0300

    add MQ and frontend

---
+++ src/SearchService/Consumers/AuctionCreatedConsumer.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using Contracts;
+using AutoMapper;
+using SearchService.Models;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionCreatedConsumer : IConsumer<AuctionCreated>
+{
+    private readonly IMapper _mapper;
+
+    public AuctionCreatedConsumer(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    public async Task Consume(ConsumeContext<AuctionCreated> context)
+    {
+        System.Console.WriteLine("--> Consuming auction created: " + context.Message.Id);
+
+        var item = _mapper.Map<Item>(context.Message);
+
+        if (item.Model == "Foo") throw new ArgumentException("Cannot sell cars with the name of Foo");
+
+        await item.SaveAsync();        
+    }
+}
