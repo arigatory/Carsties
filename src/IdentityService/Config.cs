6acede2c16b5d9da20ced9a8f2129b1267112150
Ivan Panchenko <arigatory@gmail.com>
2023-10-02 08:33:21 +0300

    add new client in identity

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -29,14 +29,18 @@ public static class Config
                 RedirectUris = { "https://www.getpostman.com/oath2/callback" },
                 ClientSecrets = { new Secret("NotASecret".Sha256()) },
                 AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-
-
-
-
-                // FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                // PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                // AllowOfflineAccess = true,
+            },
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = { "http://localhost:3000/api/auth/callback/id-server" },
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "auctionApp" },
+                AccessTokenLifetime = 3600*24*30
             },
         };
 }
