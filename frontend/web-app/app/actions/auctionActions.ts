e0d4d02031817d4874446a5bea4c81ea39180aeb
Ivan Panchenko <arigatory@gmail.com>
2024-02-27 01:58:06 +0300

    add page size

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -3,9 +3,9 @@
 import { Auction, PagedResult } from "@/types";
 
 export async function getData(
-  pageNumber: number = 1
+  pageNumber: number, pageSize: number
 ): Promise<PagedResult<Auction>> {
-  const res = await fetch(`http://localhost:6001/search?pageSize=4&pageNumber=${pageNumber}`);
+  const res = await fetch(`http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${pageNumber}`);
 
   if (!res.ok) throw new Error("Failed to fetch data");
 
