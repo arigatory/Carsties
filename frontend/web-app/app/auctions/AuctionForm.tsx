9e6f999dce2c419c226033af17c953e3ef6cef4c
Ivan Panchenko <arigatory@gmail.com>
2024-03-07 00:08:54 +0300

    done crud on front for auctions

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -1,45 +1,138 @@
-'use client'
+"use client";
 
 import { Button, TextInput } from "flowbite-react";
-import React from "react";
+import React, { useEffect } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import Input from "../components/Input";
+import DateInput from "../components/DateInput";
+import { createAuction, updateAuction } from "../actions/auctionActions";
+import { usePathname, useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import { Auction } from "@/types";
 
-export default function AuctionForm() {
+type Props = {
+  auction?: Auction;
+};
+
+export default function AuctionForm({ auction }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+
   const {
-    register,
+    control,
     handleSubmit,
     setFocus,
-    formState: { isSubmitting, isValid, isDirty, errors },
-  } = useForm();
+    reset,
+    formState: { isSubmitting, isValid },
+  } = useForm({
+    mode: "onTouched",
+  });
 
-  function onSubmit(data: FieldValues) {
-    console.log(data);
+  useEffect(() => {
+    if (auction) {
+      const { make, model, color, mileage, year } = auction;
+      reset({ make, model, color, mileage, year });
+    }
+    setFocus("make");
+  }, [setFocus]);
+
+  async function onSubmit(data: FieldValues) {
+    try {
+      let id = "";
+      let res;
+      if (pathname === "/auctions/create") {
+        res = await createAuction(data);
+        id = res.id;
+      } else {
+        if (auction) {
+          res = await updateAuction(data, auction.id);
+          id = auction.id;
+        }
+      }
+      if (res.error) {
+        throw res.error;
+      }
+      router.push(`/auctions/details/${id}`);
+    } catch (error: any) {
+      toast.error(error.status + " " + error.message);
+    }
   }
   return (
     <form className=" flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
-      <div className=" mb-3 block">
-        <TextInput
-          {...register("make", { required: "Make is required" })}
-          placeholder="Make"
-          color={errors?.make && "failure"}
-          helperText={errors.make?.message as string}
+      <Input
+        label="Make"
+        name="make"
+        control={control}
+        rules={{ required: "Make is required" }}
+      />
+      <Input
+        label="Model"
+        name="model"
+        control={control}
+        rules={{ required: "Model is required" }}
+      />
+      <Input
+        label="Color"
+        name="color"
+        control={control}
+        rules={{ required: "Year is required" }}
+      />
+
+      <div className=" grid grid-cols-2 gap-3">
+        <Input
+          label="Year"
+          name="year"
+          type="number"
+          control={control}
+          rules={{ required: "Year is required" }}
+        />
+
+        <Input
+          label="Mileage"
+          name="mileage"
+          type="number"
+          control={control}
+          rules={{ required: "Mileage is required" }}
         />
       </div>
-      <div className=" mb-3 block">
-        <TextInput
-          {...register("model", { required: "Model is required" })}
-          placeholder="Model"
-          color={errors?.model && "failure"}
-          helperText={errors.model?.message as string}
-        />
-      </div>
+
+      {pathname === "/auctions/create" && (
+        <>
+          <Input
+            label="Image URL"
+            name="imageUrl"
+            control={control}
+            rules={{ required: "Image URL is required" }}
+          />
+
+          <div className=" grid grid-cols-2 gap-3">
+            <Input
+              label="Reserve Price (enter 0 if no reserve)"
+              name="reservePrice"
+              type="number"
+              control={control}
+              rules={{ required: "Reserve price is required" }}
+            />
+
+            <DateInput
+              label="Auction and date/time"
+              name="auctionEnd"
+              control={control}
+              dateFormat="dd MMMM yyyy h:mm a"
+              showTimeSelect
+              rules={{ required: "Auction end date is required" }}
+            />
+          </div>
+        </>
+      )}
+
       <div className=" flex justify-between">
         <Button outline color="gray">
           Cancel
         </Button>
         <Button
           isProcessing={isSubmitting}
-          // disabled={!isValid}
+          disabled={!isValid}
           type="submit"
           outline
           color="success"
